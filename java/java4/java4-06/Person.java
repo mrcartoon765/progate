82101f8949bb2d8deb19088cdcf5e87a6bcd8bca
mrcartoon765 <mrcartoon765@gmail.com>
2021-09-06 14:31:54 +0900

    java for practice 4-06

---
+++ java/java4/java4-06/Person.java
@@ -0,0 +1,11 @@
+class Person {
+	public String name;
+
+	Person() {
+		System.out.println("インスタンスが生成されました");
+	}
+
+	public void hello() {
+		System.out.println("こんにちは、私は" + this.name + "です");
+	}
+}
