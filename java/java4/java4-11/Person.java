295dbed238731fe219a1f99514f21f51e65b699b
mrcartoon765 <mrcartoon765@gmail.com>
2021-09-06 15:10:34 +0900

    java for practice 4-11

---
+++ java/java4/java4-11/Person.java
@@ -0,0 +1,22 @@
+class Person {
+	public String firstName;
+	public String lastName;
+	public int age;
+	public double height;
+	public double weight;
+
+	Person(String firstName, String lastName, int age, double height, double weight) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public String fullName(){
+		return this.firstName +""+ this.lastName;
+	}
+	public double bmi(){
+		return this.weight / this.height / this.height;
+}
+}
