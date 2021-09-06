279a683562d683e58878d1edafab549bd65eaa93
mrcartoon765 <mrcartoon765@gmail.com>
2021-09-06 13:51:47 +0900

    java for practice 4-04

---
+++ java/java4/java4-04/Main.java
@@ -0,0 +1,10 @@
+class Main {
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		person1.name = "Kate Jones";
+		System.out.println(person1.name);
+		Person person2 = new Person();
+		person2.name = "John Christopher Smith";
+		System.out.println(person2.name);
+	}
+}
