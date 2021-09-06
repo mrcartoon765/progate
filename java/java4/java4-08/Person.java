956e024e0c1e287aec8fc72134341d117cec7d33
mrcartoon765 <mrcartoon765@gmail.com>
2021-09-06 14:49:43 +0900

    java for practice 4-10

---
+++ java/java4/java4-08/Person.java
@@ -0,0 +1,16 @@
+class Person {
+	public String firstName;
+	public String lastName;
+	public int age;
+	public double height;
+	public double weight;
+
+	Person(String firstName, String lastName, int age, double height, double weight) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+
+}
