18f5ffd1662eed8c369e5b34c586456a72b7ddd3
mrcartoon765 <mrcartoon765@gmail.com>
2021-09-09 11:03:11 +0900

    java-dojo-2-8

--- java/dojo2/java-1-9/Car.java
+++ java/dojo2/java-1-9/Car.java
@@ -10,23 +10,43 @@ class Car {
 	}
 
 	public void printData() {
+		System.out.println("【車の情報】");
 		System.out.println("名前：" + this.name);
 		System.out.println("色：" + this.color);
 		System.out.println("走行距離：" + this.distance + "km");
 		System.out.println("ガソリン量：" + this.fuel + "L");
+		System.out.println("-----------------");
 	}
 
 	public int run(int car_range) {
+		System.out.print("走る距離を入力してください：");
 		System.out.println(car_range + "km走ります");
-		if(fuel > car_range){
+		if (car_range < fuel) {
 			System.out.println("走行距離：" + car_range + "km");
-			System.out.println("ガソリン量：" + (fuel - car_range) + "L");
+			fuel = fuel - car_range;
+			System.out.println("ガソリン量：" + fuel + "L");
+			System.out.println("-----------------");
 			return car_range;
-		}else{
+		} else {
 			System.out.println("ガソリンが足りません");
 			System.out.println("走行距離：" + distance + "km");
 			System.out.println("ガソリン量：" + fuel + "L");
+			System.out.println("-----------------");
 			return distance;
 		}
+	}
+
+	public void charge(int charge_fuel) {
+		System.out.println(charge_fuel + "L給油します");
+		if (charge_fuel <= 0) {
+			System.out.println("給油できません");
+		} else if (100 <= (charge_fuel + fuel)) {
+			System.out.println("満タンまで給油します");
+			fuel = 100;
+		} else {
+			fuel += charge_fuel;
+		}
+		System.out.println("ガソリン量：" + fuel + "L");
+
 	}
 }
--- java/dojo2/java-1-9/Main.java
+++ java/dojo2/java-1-9/Main.java
@@ -4,21 +4,17 @@ class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Bicycle bicycle = new Bicycle("ビアンキ", "緑", 0);
-		System.out.println("【自転車の情報】");
 		bicycle.printData();
 		System.out.println("走る距離を入力してください：");
 		int bicycle_range = scanner.nextInt();
 		bicycle.run(bicycle_range);
-		System.out.println("-----------------");
 		Car car = new Car("フェラーリ", "赤");
-		System.out.println("【車の情報】");
 		car.printData();
-		System.out.println("-----------------");
 		System.out.print("走る距離を入力してください：");
 		int car_range = scanner.nextInt();
 		car.run(car_range);
-
-
+		System.out.print("給油する量を入力してください：");
+		int charge_fuel = scanner.nextInt();
+		car.charge(charge_fuel);
 	}
 }
-
